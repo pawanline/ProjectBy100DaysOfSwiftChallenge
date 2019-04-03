64b2edb7284b4992511b52889a3b740999ceea35
pawan421 <pawan@appster.in>
2019-04-03 08:56:51 +0530

    Project 16 added : with mmap view and annotation

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Pawan Kumar on 29/03/19.
+//  Copyright © 2019 PawanShivHari inc. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+
+    
+    override func didMove(to view: SKView) {
+        
+        // Get label node from scene and store it for use later
+    
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+}
---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,23 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Pawan Kumar on 03/04/19.
+//  Copyright © 2019 PawanShivHari inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject,MKAnnotation {
+
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
