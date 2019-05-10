459d9bf1307c3a03d17acc494c7d8ba19e9b542f
pawan421 <pawan@appster.in>
2019-05-10 09:28:20 +0530

    no message

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     @IBOutlet var secretTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
+        title = "Nothing to see here"
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
     }
 
     @IBAction func authenticateTapped(_ sender: Any) {
+        
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identifiy Yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] (success, authenticationError) in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+                        //error
+                        let ac = UIAlertController(title: "Authentication Failed", message: "you couldn;t verified please try again", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            // no biometry
+            
+            let ac = UIAlertController(title: "Biometry Unavailable", message: "Your device is not configured for biometric authentication", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            self.present(ac, animated: true)
+        }
+        
+        unlockSecretMessage()
     }
     
     
@@ -42,6 +74,25 @@ class ViewController: UIViewController {
         secretTextView.scrollRangeToVisible(selectedRange)
         
         
+    }
+    
+    
+    func unlockSecretMessage() {
+        secretTextView.isHidden = false
+        title = "Secret Stuff!"
+        
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+            secretTextView.text = text
+        }
+    }
+    
+    @objc func saveSecretMessage() {
+        guard secretTextView.isHidden == false  else {  return }
+        
+        KeychainWrapper.standard.set(secretTextView.text, forKey: "SecretMessage")
+        secretTextView.resignFirstResponder()
+        secretTextView.isHidden = true
+        title = "Nothing to see here"
     }
 }
 
